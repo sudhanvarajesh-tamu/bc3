a1f2127e4037df7db66fc2e78c49f9619969a1df
Sudhanva Rajesh <sudhanva0001@gmail.com>
2025-02-05 20:03:31 -0600

    added oauth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  ENV['GOOGLE_OAUTH_CLIENT_ID'] = '276005909631-c3ohlghigruu86ddrp5j8shvtasu2o59.apps.googleusercontent.com'
+  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
 end
